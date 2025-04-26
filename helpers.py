02abae294b57c4f0639a4a75c40d40bfce66810b
Mohamed Gomaa <m.gomaa.abd.mm@gmail.com>
2025-04-26 08:56:24 +0300

    npl test

--- helpers.py
+++ helpers.py
@@ -34,13 +34,10 @@ def edit_agent_value(value, field):
         return pd.to_datetime(value).strftime('%d/%m/%Y')
 
     elif str(field).strip().upper() == "NATIONALITY":
-        return "KEN"
+        return "NPL"
     
     elif str(field).strip().upper() == "COUNTRY OF ISSUE" or str(field).strip().upper() == "PASSPORT PLACE(EN)":
         return value.upper()
-    
-    elif str(field).strip().upper() == "BIRTH PLACE":
-        return value.upper().split(", KEN")[0].split("KEN")[0].strip()
 
     elif str(field).strip().upper() == "GENDER":
         return value.upper()[0]
